e1ca87c2aa3d2d7228f53058d82a93dd02800337
Siddall, Jake(jsiddall) <Jake.Siddall@cmegroup.com>
2014-05-06 00:12:30 -0500

    finishes update to background frame (sizing within resizing now works correctly for all types - need customer feedback), social icons added, footer added

---
+++ resources/js/colorbox.js
@@ -0,0 +1,9 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  // $('.flexslider').flexslider({
+  //   animation: "slide"
+  // });
+ //  $('.nav .dropdown').hover(function() {
+ //  		console.log("we are hovering")
+	// })
+});
